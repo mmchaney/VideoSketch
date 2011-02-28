3e3e8f9d015c4506458bbd4bd62d3a5f17ea421d
mmchaney <markusmc@gmail.com>
2011-02-28 15:00:20 +0100

    Switched to JSHint

--- videosketch.js
+++ videosketch.js
@@ -1,16 +1,16 @@
 /*
  * VideoSketch. Telestrator like freehand sketching 
  * on HTML5 video. Functionality as seen during sports broadcasts.
- * 2010-11-21
  *
  * By Markus Messner-Chaney, http://mmessnerchaney.com
  * Public Domain.
  * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
  */
-
-/*jslint white: true, browser: true, devel: true, windows: true, onevar: true, undef: true, eqeqeq: true, 
-bitwise: true, regexp: true, newcap: true, immed: true, maxlen: 120, indent: 2 */
-/*global videosketch, Element, Function, Math, Object */
+ 
+/*jshint browser: true, boss: true, curly: true, eqeqeq: true, immed: true, 
+  newcap: true, noarg: true, noempty: true, overvar: true, undef: true, 
+  white: true, indent: 2 */
+/*global glint, Element, Function, Object */
 
 (function (window) {
 
